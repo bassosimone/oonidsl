f6315d38c346d9fa3ead6cae8bd61253243c42f9
Simone Basso <bassosimone@gmail.com>
2022-11-29 12:41:11 +0100

    chore: add unmodified ooni/probe-cli libraries
    We're importing these libraries from ooni/probe-cli@6591fe56.

---
+++ internal/netxlite/utls_test.go
@@ -0,0 +1,161 @@
+package netxlite
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"net"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/apex/log"
+	"github.com/ooni/probe-cli/v3/internal/model/mocks"
+	utls "gitlab.com/yawning/utls.git"
+)
+
+func TestNewTLSHandshakerUTLS(t *testing.T) {
+	th := NewTLSHandshakerUTLS(log.Log, &utls.HelloChrome_83)
+	logger := th.(*tlsHandshakerLogger)
+	if logger.DebugLogger != log.Log {
+		t.Fatal("invalid logger")
+	}
+	configurable := logger.TLSHandshaker.(*tlsHandshakerConfigurable)
+	if configurable.NewConn == nil {
+		t.Fatal("expected non-nil NewConn")
+	}
+}
+
+func TestUTLSConn(t *testing.T) {
+	t.Run("Handshake", func(t *testing.T) {
+		t.Run("not interrupted with success", func(t *testing.T) {
+			ctx := context.Background()
+			conn := &utlsConn{
+				testableHandshake: func() error {
+					return nil
+				},
+			}
+			err := conn.HandshakeContext(ctx)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+
+		t.Run("not interrupted with failure", func(t *testing.T) {
+			expected := errors.New("mocked error")
+			ctx := context.Background()
+			conn := &utlsConn{
+				testableHandshake: func() error {
+					return expected
+				},
+			}
+			err := conn.HandshakeContext(ctx)
+			if !errors.Is(err, expected) {
+				t.Fatal("not the error we expected", err)
+			}
+		})
+
+		t.Run("interrupted", func(t *testing.T) {
+			wg := sync.WaitGroup{}
+			wg.Add(1)
+			sigch := make(chan interface{})
+			ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+			defer cancel()
+			conn := &utlsConn{
+				testableHandshake: func() error {
+					defer wg.Done()
+					<-sigch
+					return nil
+				},
+			}
+			err := conn.HandshakeContext(ctx)
+			if !errors.Is(err, context.DeadlineExceeded) {
+				t.Fatal("not the error we expected", err)
+			}
+			close(sigch)
+			wg.Wait()
+		})
+
+		t.Run("with panic", func(t *testing.T) {
+			wg := sync.WaitGroup{}
+			wg.Add(1)
+			ctx := context.Background()
+			conn := &utlsConn{
+				testableHandshake: func() error {
+					defer wg.Done()
+					panic("mascetti")
+				},
+			}
+			err := conn.HandshakeContext(ctx)
+			if !errors.Is(err, ErrUTLSHandshakePanic) {
+				t.Fatal("not the error we expected", err)
+			}
+			wg.Wait()
+		})
+	})
+
+	t.Run("NetConn", func(t *testing.T) {
+		factory := newConnUTLS(&utls.HelloChrome_70)
+		conn := &mocks.Conn{}
+		tconn, err := factory(conn, &tls.Config{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		// TODO(https://github.com/ooni/probe/issues/2222): we cannot avoid
+		// castedTconn until we use oocrypto >= v0.2 which uses go1.19. In turn,
+		// we cannot use go1.19 as our main version until we upgrade psiphon
+		// such that it builds using go1.19, which is the issue in #2222.
+		castedTconn := tconn.(*utlsConn)
+		if castedTconn.NetConn() != conn {
+			t.Fatal("NetConn is not WAI")
+		}
+	})
+}
+
+func Test_newConnUTLSWithHelloID(t *testing.T) {
+	tests := []struct {
+		name        string
+		config      *tls.Config
+		cid         *utls.ClientHelloID
+		wantNilConn bool
+		wantErr     error
+	}{{
+		name: "with only supported fields",
+		config: &tls.Config{
+			DynamicRecordSizingDisabled: true,
+			InsecureSkipVerify:          true,
+			NextProtos:                  []string{"h3"},
+			RootCAs:                     NewDefaultCertPool(),
+			ServerName:                  "ooni.org",
+		},
+		cid:         &utls.HelloFirefox_55,
+		wantNilConn: false,
+		wantErr:     nil,
+	}, {
+		name: "with unsupported fields",
+		config: &tls.Config{
+			Time: func() time.Time {
+				return time.Now()
+			},
+		},
+		cid:         &utls.HelloChrome_58,
+		wantNilConn: true,
+		wantErr:     errUTLSIncompatibleStdlibConfig,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			conn, err := net.Dial("udp", "8.8.8.8:443") // we just need a conn
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer conn.Close()
+			got, err := newConnUTLSWithHelloID(conn, tt.config, tt.cid)
+			if !errors.Is(err, tt.wantErr) {
+				t.Fatal("unexpected err", err)
+			}
+			if got != nil && tt.wantNilConn {
+				t.Fatal("expected nil conn here")
+			}
+		})
+	}
+}
---
+++ internal/runtimex/runtimex_test.go
@@ -0,0 +1,95 @@
+package runtimex_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/ooni/probe-cli/v3/internal/runtimex"
+)
+
+func TestPanicOnError(t *testing.T) {
+	badfunc := func(in error) (out error) {
+		defer func() {
+			out = recover().(error)
+		}()
+		runtimex.PanicOnError(in, "we expect this assertion to fail")
+		return
+	}
+
+	t.Run("error is nil", func(t *testing.T) {
+		runtimex.PanicOnError(nil, "this assertion should not fail")
+	})
+
+	t.Run("error is not nil", func(t *testing.T) {
+		expected := errors.New("mocked error")
+		if !errors.Is(badfunc(expected), expected) {
+			t.Fatal("not the error we expected")
+		}
+	})
+}
+
+func TestAssert(t *testing.T) {
+	badfunc := func(in bool, message string) (out error) {
+		defer func() {
+			out = recover().(error)
+		}()
+		runtimex.Assert(in, message)
+		return
+	}
+
+	t.Run("assertion is true", func(t *testing.T) {
+		runtimex.Assert(true, "this assertion should not fail")
+	})
+
+	t.Run("assertion is false", func(t *testing.T) {
+		message := "mocked error"
+		err := badfunc(false, message)
+		if err == nil || err.Error() != message {
+			t.Fatal("not the error we expected", err)
+		}
+	})
+}
+
+func TestPanicIfTrue(t *testing.T) {
+	badfunc := func(in bool, message string) (out error) {
+		defer func() {
+			out = recover().(error)
+		}()
+		runtimex.PanicIfTrue(in, message)
+		return
+	}
+
+	t.Run("assertion is false", func(t *testing.T) {
+		runtimex.PanicIfTrue(false, "this assertion should not fail")
+	})
+
+	t.Run("assertion is true", func(t *testing.T) {
+		message := "mocked error"
+		err := badfunc(true, message)
+		if err == nil || err.Error() != message {
+			t.Fatal("not the error we expected", err)
+		}
+	})
+}
+
+func TestPanicIfNil(t *testing.T) {
+	badfunc := func(in interface{}, message string) (out error) {
+		defer func() {
+			out = recover().(error)
+		}()
+		runtimex.PanicIfNil(in, message)
+		return
+	}
+
+	t.Run("value is not nil", func(t *testing.T) {
+		runtimex.PanicIfNil(false, "this assertion should not fail")
+	})
+
+	t.Run("value is nil", func(t *testing.T) {
+		message := "mocked error"
+		err := badfunc(nil, message)
+		if err == nil || err.Error() != message {
+			t.Fatal("not the error we expected", err)
+		}
+	})
+}
