2b4756cca2f091986d1011c363bc5b2c3c3cd9e9
kelmenhorst <k.elmenhorst@mailbox.org>
2022-12-14 12:58:14 +0100

    fix(signal): run test domains in parralel

--- cmd/signal/measure.go
+++ cmd/signal/measure.go
@@ -14,24 +14,6 @@ import (
 
 // measure is the top-level measurement algorithm.
 func measure(ctx context.Context, state *measurementState) error {
-	// run DCs measurements in background
-	state.wg.Add(1)
-
-	// dnslookup
-	go measureTargets(ctx, state)
-
-	// wait for measurements to terminate
-	state.wg.Wait()
-
-	// make sure we fail the measurement if the main
-	// context is cancelled (e.g., because the user
-	// has hit ^C and has forced an early termination)
-	return ctx.Err()
-}
-
-func measureTargets(ctx context.Context, state *measurementState) {
-	defer state.wg.Done()
-
 	domains := []string{
 		"textsecure-service.whispersystems.org",
 		"storage.signal.org",
@@ -40,106 +22,132 @@ func measureTargets(ctx context.Context, state *measurementState) {
 		"cdn2.signal.org",
 		"sfu.voip.signal.org",
 	}
+	errch := make(chan error, len(domains))
 
+	for _, domain := range domains {
+		state.wg.Add(1)
+		go measureTarget(ctx, state, domain, errch)
+	}
+
+	// wait for measurements to terminate
+	state.wg.Wait()
+
+	for {
+		select {
+		case e := <-errch:
+			if e != nil {
+				state.tk.setResultFailure(e)
+				return ctx.Err()
+			}
+		default:
+			state.tk.setResultSuccess()
+			return ctx.Err()
+		}
+	}
+}
+
+func measureTarget(
+	ctx context.Context,
+	state *measurementState,
+	domain string,
+	errch chan error,
+) {
+	defer state.wg.Done()
+	errch <- doMeasureTarget(ctx, state, domain)
+}
+
+func doMeasureTarget(
+	ctx context.Context,
+	state *measurementState,
+	domain string,
+) error {
+	// describe the DNS measurement input
+	dnsInput := dslx.DNSLookupInput(
+		dslx.DomainName(domain),
+		dslx.DNSLookupOptionIDGenerator(state.idGen), // do I have to increment this?
+		dslx.DNSLookupOptionLogger(state.logger),
+		dslx.DNSLookupOptionZeroTime(state.zeroTime),
+	)
 	// construct getaddrinfo resolver
 	lookup := dslx.DNSLookupGetaddrinfo()
+	// run the DNS Lookup
+	dnsResult := lookup.Apply(ctx, dnsInput)
+
+	// extract and merge observations with the test keys
+	state.tk.mergeObservations(dslx.ExtractObservations(dnsResult)...)
+
+	// if the lookup has failed we return
+	if dnsResult.IsErr() {
+		return dnsResult.UnwrapErr()
+	}
+
+	// obtain a unique set of IP addresses w/o bogons inside it
+	ipAddrs := dslx.AddressSet(dnsResult).RemoveBogons()
+
+	// create the set of endpoints
+	endpoints := ipAddrs.ToEndpoints(
+		dslx.EndpointNetwork("tcp"),
+		dslx.EndpointPort(443),
+		dslx.EndpointOptionDomain(domain),
+		dslx.EndpointOptionIDGenerator(state.idGen),
+		dslx.EndpointOptionLogger(state.logger),
+		dslx.EndpointOptionZeroTime(state.zeroTime),
+	)
+
+	// count the number of successes
+	successes := dslx.Counter[*dslx.HTTPRequestResultState]()
 
 	// create the established connections pool
 	connpool := &dslx.ConnPool{}
 	defer connpool.Close()
 
+	// create the certificate pool
 	certPool, err := newCertPool()
 	if err != nil {
 		// TODO
 	}
 
-	var successes *dslx.CounterState[*dslx.HTTPRequestResultState]
-	var httpsResults []fx.Result[*dslx.HTTPRequestResultState]
+	// create function for the 443/tcp/tls/https measurement
+	httpsFunction := fx.ComposeResult6(
+		dslx.TCPConnect(connpool),
+		dslx.TLSHandshake(
+			connpool,
+			dslx.TLSHandshakeOptionRootCAs(certPool),
+		),
+		dslx.HTTPTransportTLS(),
+		dslx.HTTPJustUseOneConn(), // TODO: do we want this?
+		dslx.HTTPRequest(),
+		successes.Func(), // number of times we arrive here
+	)
 
-	for _, d := range domains {
-		// describe the DNS measurement input
-		dnsInput := dslx.DNSLookupInput(
-			dslx.DomainName(d),
-			dslx.DNSLookupOptionIDGenerator(state.idGen), // do I have to increment this?
-			dslx.DNSLookupOptionLogger(state.logger),
-			dslx.DNSLookupOptionZeroTime(state.zeroTime),
-		)
-		// run the DNS Lookup
-		dnsResults := fx.Map(
-			ctx,
-			fx.Parallelism(3),
-			lookup,
-			dnsInput,
-		)
-		// extract and merge observations with the test keys
-		state.tk.mergeObservations(dslx.ExtractObservations(dnsResults...)...)
+	// run 443/tcp/tls/https measurement
+	httpsResults := fx.Map(
+		ctx,
+		fx.Parallelism(2),
+		httpsFunction,
+		endpoints...,
+	)
 
-		// if the lookup has failed mark the whole web measurement as failed
-		// TODO
+	// extract and merge observations with the test keys
+	state.tk.mergeObservations(dslx.ExtractObservations(httpsResults...)...)
 
-		// obtain a unique set of IP addresses w/o bogons inside it
-		ipAddrs := dslx.AddressSet(dnsResults...).RemoveBogons()
-
-		// create the set of endpoints
-		endpoints := ipAddrs.ToEndpoints(
-			dslx.EndpointNetwork("tcp"),
-			dslx.EndpointPort(443),
-			dslx.EndpointOptionDomain(d),
-			dslx.EndpointOptionIDGenerator(state.idGen),
-			dslx.EndpointOptionLogger(state.logger),
-			dslx.EndpointOptionZeroTime(state.zeroTime),
-		)
-
-		// count the number of successes
-		successes = dslx.Counter[*dslx.HTTPRequestResultState]()
-
-		// create function for the 443/tcp/tls/https measurement
-		httpsFunction := fx.ComposeResult6(
-			dslx.TCPConnect(connpool),
-			dslx.TLSHandshake(
-				connpool,
-				dslx.TLSHandshakeOptionRootCAs(certPool),
-			),
-			dslx.HTTPTransportTLS(),
-			dslx.HTTPJustUseOneConn(), // stop subsequent connections
-			dslx.HTTPRequest(),
-			successes.Func(), // number of times we arrive here
-		)
-
-		// run 443/tcp/tls/https measurement
-		httpsResults = fx.Map(
-			ctx,
-			fx.Parallelism(2),
-			httpsFunction,
-			endpoints...,
-		)
-
-		// extract and merge observations with the test keys
-		state.tk.mergeObservations(dslx.ExtractObservations(httpsResults...)...)
-	}
-
-	// if we saw successes, then it's not blocked
-	// TODO: re-define what success means here!
+	// if we saw successes, then this domain is not blocked
 	if successes.Value() > 0 {
-		state.tk.setResultSuccess()
-		return
+		return nil
 	}
 
 	// attempt to set a meaningful error, if that's possible
 	if err := dslx.FirstErrorExcludingBrokenIPv6Errors(httpsResults...); err != nil {
-		state.tk.setResultFailure(err)
-		return
+		return err
 	}
 
 	// otherwise fallback to whatever is the first error
 	if err := dslx.FirstError(httpsResults...); err != nil {
-		state.tk.setResultFailure(err)
-		return
+		return err
 	}
 
 	// the last resort is to set an unknown failure error
-	state.tk.setResultFailure(netxlite.ErrUnknown)
-
+	return netxlite.ErrUnknown
 }
 
 // setResultSuccess sets the result of the experiment in case of success
